f4643716872d174162265a5ea5295d50d3010568
Sany655 <mazharulalam26@gmail.com>
2025-05-03 01:10:11 +0600

    clean project started

--- src/App.js
+++ src/App.js
@@ -3,34 +3,36 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import Contact from "./components/Contact/Contact";
-import Home from "./components/Home/Home";
-import Profile from "./components/Profile/Profile";
-import Projects from "./components/Projects/Projects";
-import ProjectDetail from "./components/ProjectDetail/ProjectDetail";
-import Blogs from "./components/Blogs/Blogs";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} >
-          <Route path="/" element={<Profile />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/project-detail/:id" element={<ProjectDetail />} />
-          <Route path="/blogs" element={<Blogs />} />
-        </Route>
-        <Route
-          path="*"
-          element={
-            <main className="h-screen flex justify-center items-center">
-              <p className="">404</p>
-            </main>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} >
+    //       <Route path="/" element={<Profile />} />
+    //       <Route path="/contact" element={<Contact />} />
+    //       <Route path="/projects" element={<Projects />} />
+    //       <Route path="/project-detail/:id" element={<ProjectDetail />} />
+    //       <Route path="/blogs" element={<Blogs />} />
+    //     </Route>
+    //     <Route path="/admin" element={<AdminLayout />} >
+    //       <Route path="/" element={<Profile />} />
+    //       <Route path="/contact" element={<Contact />} />
+    //       <Route path="/projects" element={<Projects />} />
+    //       <Route path="/project-detail/:id" element={<ProjectDetail />} />
+    //       <Route path="/blogs" element={<Blogs />} />
+    //     </Route>
+    //     <Route
+    //       path="*"
+    //       element={
+    //         <main className="h-screen flex justify-center items-center">
+    //           <p className="">404</p>
+    //         </main>
+    //       }
+    //     />
+    //   </Routes>
+    // </BrowserRouter>
+    <div className="text-blue-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere inventore voluptate quas maxime eius nobis consequatur eaque vero aut eligendi odio tenetur soluta, in veniam iusto suscipit temporibus dicta! In assumenda, nostrum fuga sint quasi corporis explicabo ea culpa placeat velit ab, rerum autem et tempore, saepe natus aliquid mollitia! Ab explicabo voluptates sint inventore debitis tenetur, perspiciatis nesciunt, quis quibusdam mollitia veritatis modi reiciendis quisquam sapiente minus facilis eaque at? Dolores atque corrupti quae quisquam aut in excepturi! Iusto repellendus atque suscipit deleniti. Molestias dolore cumque nesciunt repellat harum optio! Dignissimos possimus accusantium ex dolor esse sapiente neque dolorum.</div>
   );
 }
 
